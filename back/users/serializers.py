763caefcd7212f2c1c534fcc9659a783c0827359
Khanbin <totoshka1588@gmail.com>
2026-04-18 21:29:55 +0500

    delete redundant folder

--- back/users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'birth_date', 'is_verified']
